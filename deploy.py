f1a0c6449ec5d593fbdf94f06020ceb5d6d82690
Zion Ukpong <105987372+tegacodess@users.noreply.github.com>
2025-07-18 11:10:12 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -30,16 +30,16 @@ age_groups = 'Child' if age <= 12 else 'Adolescence' if age <= 18 else 'Adult' i
 # predict button
 if st.button("Predict Calories Burned"):
     data = {
-        "Gender": gender,
-        "Age": age,
-        "Height": height,
-        "Weight": weight,
-        "Duration": duration,
-        "Heart_Rate": heart_rate,
-        "Body_Temp": body_temp,
-        "BMI": bmi,
-        "BMI Category": bmi_category,
-        "Age Groups": age_groups
+        "Gender": [gender],
+        "Age": [age],
+        "Height": [height],
+        "Weight": [weight],
+        "Duration": [duration],
+        "Heart_Rate": [heart_rate],
+        "Body_Temp": [body_temp],
+        "BMI": [bmi],
+        "BMI Category": [bmi_category],
+        "Age Groups": [age_groups]
     }
     # convert to dataframe
     df = pd.DataFrame(data)
